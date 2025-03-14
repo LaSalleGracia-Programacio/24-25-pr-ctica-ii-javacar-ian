cb12b1bbbc88fc4b293d483b859e0cadb027b3b7
Iannnnnnn12 <ian.gimeno@gracia.lasalle.cat>
2025-03-14 13:33:17 +0100

    Codi clase Roda

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -1,4 +1,19 @@
 package org.JavaCar;
 
 public class Roda {
+    private String Marca;
+    private int diametre;
+
+    public Roda(String marca, int diametre) {
+        this.Marca = marca;
+        this.diametre = diametre;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public int getDiametre() {
+        return diametre;
+    }
 }
