7342c1d6955b5b91e574d8708b6b63fdb1bcb186
Iannnnnnn12 <ian.gimeno@gracia.lasalle.cat>
2025-03-14 13:14:10 +0100

    Atributs Clase Vehicle

---
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -0,0 +1,12 @@
+package org.JavaCar;
+
+public abstract class Vehicle {
+    private String Matricula;
+    private String Marca;
+    private String Model;
+    private double preuBase;
+    private String motor;
+    private String rodes;
+}
+
+
