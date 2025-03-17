f5e0de670cc9a6cfc9a151212e453b56bfcb8bd1
Iannnnnnn12 <ian.gimeno@gracia.lasalle.cat>
2025-03-17 23:06:00 +0100

    correccio errors

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,33 +1,33 @@
 package org.JavaCar;
 
 public abstract class Vehicle implements Llogable {
-    private String Matricula;
-    private String Marca;
-    private String Model;
-    private double preuBase;
-    private Motor motor;
-    private Roda[] rodes;
+    protected String matricula;
+    protected String marca;
+    protected String model;
+    protected double preuBase;
+    protected Motor motor;
+    protected Roda[] rodes;
 
 
-    public Vehicle(String Matricula, String Marca, String Model, double preuBase, Motor motor,Roda[] rodes) {
-        this.Matricula = Matricula;
-        this.Marca = Marca;
-        this.Model = Model;
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor,Roda[] rodes) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
         this.preuBase = preuBase;
         this.motor = motor;
         this.rodes = rodes;
     }
 
     public String getMatricula() {
-        return Matricula;
+        return matricula;
     }
 
     public String getMarca() {
-        return Marca;
+        return marca;
     }
 
     public String getModel() {
-        return Model;
+        return model;
     }
 
     public double getPreuBase() {
